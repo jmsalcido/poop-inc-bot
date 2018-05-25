6551e701fdfe90505672649997c80134b3783dbd
jmsalcido <jose@otfusion.org>
2018-05-24 17:12:08 -0700

    ✨ separate some stuff and follow redirect in response

---
+++ bathfinder.rb
@@ -0,0 +1,66 @@
+require 'dotenv'
+Dotenv.load
+
+require "http"
+require "json"
+require "pry"
+
+class BathFinder
+
+    def initialize
+        @floors = Hash.new
+        @floors[1] = "Planta baja"
+        @floors[2] = "Segundo piso"
+        @floors[3] = "Tercer piso"
+    end
+
+    def is_valid(floor)
+        [1,2,3].include? floor
+    end
+
+    def finding_text(floor)
+        "trying to find bathroom in floor: *#{floor}* :toilet:"
+    end
+
+    def invalid_floor(floor)
+        ":toilet: *#{floor}* is not valid bato."
+    end
+
+    def find_bath(floor)
+        found = find_bath_in_api(floor)
+
+        if found == nil
+            text = not_found_text
+        else
+            if found["status"] == "0" # status = 0 // vacant.
+                text = found_available_text
+            else
+                text = found_not_available_text
+            end
+        end
+
+        text
+    end
+
+    private
+
+    def find_bath_in_api(floor)
+        poop = HTTP.follow.get(ENV['POOP_INC_URL'])
+        availability = JSON.parse(poop.body.to_s)
+
+        availability.find { |info| info["name"] == @floors[floor] }
+    end
+
+    def not_found_text
+        "sorry bato, could not found that bathroom, check with @c13m3n7 or @jmsalcido"
+    end
+
+    def found_available_text
+        ":toilet: *[AVAILABLE]* :white_check_mark:"
+    end
+
+    def found_not_available_text
+        ":toilet: *[UNAVAILABLE]* :negative_squared_cross_mark: - check https://goo.gl/AeZY49"
+    end
+
+end
--- poopbot.rb
+++ poopbot.rb
@@ -16,35 +16,18 @@ require "pry"
 
 class PoopBot < SlackRubyBot::Bot
 
-    floors = Hash.new
-    floors[1] = "Planta baja"
-    floors[2] = "Segundo piso"
-    floors[3] = "Tercer piso"
-
     match(/floor\s*(?<floor>\d*)/i) do |client, data, match|
+        finder = BathFinder
         floor = match[:floor].to_i
 
-        text = "trying to find bathroom in floor: *#{floor}* :toilet:"
+        text = finder.finding_text(floor)
 
-        if [1,2,3].include? floor
+        if finder.is_valid(floor)
             client.say(text: text, channel: data.channel)
 
-            poop = HTTP.get(ENV['POOP_INC_URL'])
-            availability = JSON.parse(poop.body.to_s)
-
-            found = availability.find { |info| info["name"] == floors[floor] }
-
-            if found == nil
-                text = "sorry bato, could not found that bathroom, check with @c13m3n7 or @jmsalcido"
-            else
-                if found["status"] == "0" # status = 0 // vacant.
-                    text = ":toilet: *[AVAILABLE]* :white_check_mark:"
-                else
-                    text = ":toilet: *[UNAVAILABLE]* :negative_squared_cross_mark: - check https://goo.gl/AeZY49"
-                end
-            end
+            text = finder.find_bath(floor)
         else
-            text = ":toilet: *#{floor}* is not valid bato."
+            text = finder.invalid_floor(floor)
         end
 
         client.say(text: text, channel: data.channel)
